91b394445f042cd727f5e24312fe378029bc4f26
Chritie MPASSI <christmp19@gmail.com>
2024-05-02 14:08:35 +0100

    Implementing Authentication with NextAuth.js( the Login / Logout Functionality)

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB3b6YzW63cyz1Haizuan6q3DaLzg9DzKs",
+    authDomain: "clone-6f92b.firebaseapp.com",
+    projectId: "clone-6f92b",
+    storageBucket: "clone-6f92b.appspot.com",
+    messagingSenderId: "102874643647",
+    appId: "1:102874643647:web:0b72ae254b5176c1ef1a0a"
+};
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+
+
+// export { auth };
+// export default db;
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,12 @@ import {
     SearchIcon,
     ShoppingCartIcon,
 } from '@heroicons/react/outline';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 function Header() {
+    const { data: session } = useSession();
+    console.log(session);
+
     return (
         <header>
             {/* Top nav */}
@@ -31,15 +35,17 @@ function Header() {
 
                 {/* Right */}
                 <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-                    <div className='link'>
-                        <p>Hello Christie MP</p>
+                    <div onClick={!session ? signIn : signOut} className='cursor-pointer link'>
+                        <p className='hover:underline'>
+                            {session ? `Hello, ${session.user.name}` : 'Sign In'}
+                        </p>
                         <p className='font-extrabold md:text-sm'>Account & Lists</p>
                     </div>
-                    <div className='link'>
+                    <div className='cursor-pointer link'>
                         <p>Returns</p>
                         <p className='font-extrabold md:text-sm'>& Orders</p>
                     </div>
-                    <div className='relative link flex items-center'>
+                    <div className='cursor-pointer relative link flex items-center'>
                         <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
                         <ShoppingCartIcon className='h-10' />
                         <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
