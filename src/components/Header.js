8d0ff4372ef75ffc83ee194ffe0650319ae9a1bf
Chritie MPASSI <christmp19@gmail.com>
2024-05-02 14:47:18 +0100

    Creating the Checkout Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,11 @@ import {
     ShoppingCartIcon,
 } from '@heroicons/react/outline';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 function Header() {
     const { data: session } = useSession();
-    console.log(session);
+    const router = useRouter();
 
     return (
         <header>
@@ -19,6 +20,7 @@ function Header() {
                 {/* Logo */}
                 <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
                     <Image
+                        onClick={() => router.push('/')}
                         src='https://links.papareact.com/f90'
                         width={150}
                         height={40}
@@ -45,7 +47,7 @@ function Header() {
                         <p>Returns</p>
                         <p className='font-extrabold md:text-sm'>& Orders</p>
                     </div>
-                    <div className='cursor-pointer relative link flex items-center'>
+                    <div onClick={() => router.push('/checkout')} className='cursor-pointer relative link flex items-center'>
                         <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
                         <ShoppingCartIcon className='h-10' />
                         <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
