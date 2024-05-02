de5165a259e45072dad3a8b271bf9987143a148d
Chritie MPASSI <christmp19@gmail.com>
2024-05-02 20:08:59 +0100

    Fixing the Google Authorisation Error

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,7 +9,8 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_SECRET,
         }),
     //    Add more providers here
-    ]
+    ],
+    secret: '7ff53137d5138244e6a9d4c64b7b1879b118a1d5a0443f7fbcc6fd66cccc02cc',
 }
 
 export default NextAuth(authOptions)
