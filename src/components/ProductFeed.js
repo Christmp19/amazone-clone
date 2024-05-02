098f8a00d50a0abc39b202c7674752cd3f9bd6dc
Chritie MPASSI <christmp19@gmail.com>
2024-05-02 17:44:11 +0100

    Add the middle banner

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,8 +3,10 @@ import Product from './Product';
 
 function ProductFeed({ products }) {
     return (
-        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 md:-mt-52 mx-auto'>
+        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+
             {products
+                .splice(0,3)
                 .map(({ id,
                     title,
                     price,
@@ -13,16 +15,69 @@ function ProductFeed({ products }) {
                     category,
                     image,
                     hasPrime, }) => (
-                <Product
-                    key={id}
-                    id={id}
-                    title={title}
-                    price={price}
-                    description={description}
-                    category={category}
-                    image={image}
-                />
-            ))}
+                    <Product
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        rating={rating}
+                        description={description}
+                        category={category}
+                        image={image}
+                        hasPrime={hasPrime}
+                    />
+                ))}
+            <img className='md:col-span-full'
+                src="https://links.papareact.com/dyz"
+                alt="Middle banner" />
+            
+            <div className='md:col-span-2'>
+                {products
+                    .splice(3, 4)
+                    .map(({ id,
+                        title,
+                        price,
+                        rating,
+                        description,
+                        category,
+                        image,
+                        hasPrime, }) => (
+                        <Product
+                            key={id}
+                            id={id}
+                            title={title}
+                            price={price}
+                            rating={rating}
+                            description={description}
+                            category={category}
+                            image={image}
+                            hasPrime={hasPrime}
+                        />
+                    ))}
+            </div>
+
+            {products
+                .splice(4, products.length)
+                .map(({ id,
+                    title,
+                    price,
+                    rating,
+                    description,
+                    category,
+                    image,
+                    hasPrime, }) => (
+                    <Product
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        rating={rating}
+                        description={description}
+                        category={category}
+                        image={image}
+                        hasPrime={hasPrime}
+                    />
+                ))}
         </div>
     )
 }
