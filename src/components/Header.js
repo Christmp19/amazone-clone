392920080d75aeeac682a9268a63b51a216fca6b
Chritie MPASSI <christmp19@gmail.com>
2024-05-02 15:04:27 +0100

    Implementing Redux for Add to Basket Functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,13 @@ import {
 } from '@heroicons/react/outline';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { selectItems } from '../slices/basketSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
     const { data: session } = useSession();
     const router = useRouter();
+    const items = useSelector(selectItems)
 
     return (
         <header>
@@ -48,7 +51,9 @@ function Header() {
                         <p className='font-extrabold md:text-sm'>& Orders</p>
                     </div>
                     <div onClick={() => router.push('/checkout')} className='cursor-pointer relative link flex items-center'>
-                        <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
+                        <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
+                            {items.length}
+                        </span>
                         <ShoppingCartIcon className='h-10' />
                         <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
                     </div>
