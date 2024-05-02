91230a3161a62b36e50eee9cd8894f6d61962f85
Chritie MPASSI <christmp19@gmail.com>
2024-05-02 17:49:52 +0100

    Fixed bug

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,7 +3,7 @@ import Product from './Product';
 
 function ProductFeed({ products }) {
     return (
-        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 md:-mt-52 mx-auto'>
 
             {products
                 .slice(0,3)
